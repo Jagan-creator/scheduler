8514d7977d38ff77dbdf681eed2d5fded1ea20ba
Joel Hagan <joel.hagan.8@gmail.com>
2023-03-26 16:14:04 -0600

    create component for InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    interviewers__item: !props.selected,
+  });
+
+  return (
+    <li
+      className={interviewerClass}
+      onClick={() => props.setInterviewer(props.name)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+}
