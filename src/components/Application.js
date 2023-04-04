7d9d72777d1c2a4139ee7cefbb81dd4db9d7ab30
Joel Hagan <joel.hagan.8@gmail.com>
2023-04-03 18:55:02 -0600

    move appointments into useState and refactor schedule mapping

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,60 +5,52 @@ import axios from "axios";
 
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  {
-    id: 5,
-    time: "4pm",
-  },
-];
-
-const bookings = appointments.map((appointment) => {
-  return (
-    <Appointment
-      key={appointment.id}
-      {...appointment}
-    />
-  );
-});
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {},
+    appointments: {
+      1: {
+        id: 1,
+        time: "12pm",
+      },
+      2: {
+        id: 2,
+        time: "1pm",
+        interview: {
+          student: "Lydia Miller-Jones",
+          interviewer: {
+            id: 3,
+            name: "Sylvia Palmer",
+            avatar: "https://i.imgur.com/LpaY82x.png",
+          },
+        },
+      },
+      3: {
+        id: 3,
+        time: "2pm",
+      },
+      4: {
+        id: 4,
+        time: "3pm",
+        interview: {
+          student: "Archie Andrews",
+          interviewer: {
+            id: 4,
+            name: "Cohana Roy",
+            avatar: "https://i.imgur.com/FK8V841.jpg",
+          },
+        },
+      },
+      5: {
+        id: 5,
+        time: "4pm",
+      },
+    },
+    interviewer: "Sylvia Palmer",
   });
+
+  const dailyAppointments = [];
 
   const setDay = (day) => setState({ ...state, day });
   const setDays = (days) => setState((prev) => ({ ...prev, days }));
@@ -95,11 +87,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {bookings}
-        <Appointment
-          key="last"
-          time="5pm"
-        />
+        {Object.values(state.appointments).map((appointment) => {
+          return (
+            <Appointment
+              key={appointment.id}
+              {...appointment}
+            />
+          );
+        })}
       </section>
     </main>
   );
