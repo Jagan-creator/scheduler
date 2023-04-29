565b9e3773ff6f2349978851d3fa3c26c4bf065a
Joel Hagan <joel.hagan.8@gmail.com>
2023-04-28 21:25:20 -0600

    add cypress test for editing interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,9 +2,7 @@ describe("appointments", () => {
   // inital load procedures
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
-
     cy.visit("/");
-
     cy.contains("Monday");
   });
 
@@ -13,8 +11,21 @@ describe("appointments", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
+
     cy.contains("Save").click();
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({ force: true });
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
